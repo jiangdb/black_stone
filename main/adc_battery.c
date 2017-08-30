5838b17dbf273cff757d6e845be3e4153a5d72d2
jiangdb <jiang.db@gmail.com>
2017-08-30 15:06:25 +0800

    Change full voltage to 3.3v

--- main/adc_battery.c
+++ main/adc_battery.c
@@ -26,7 +26,7 @@ void battery_task(void* arg)
     while(1){
     	adcValue = adc1_get_voltage(ADC1_CHANNEL);
         printf("The adc1 value:%d\n",adcValue);
-        printf("battery voltage:%d\n",(adcValue*3900)/4095);
+        printf("battery voltage:%d\n",(adcValue*3300)/4095);
 
         if (adcValue > BATTERY_PERCENTAGE_75) {
         	setBatteryLevel(BATTERY_LEVEL_3);
@@ -37,7 +37,7 @@ void battery_task(void* arg)
         }else if (adcValue > BATTERY_PERCENTAGE_0) {
         	setBatteryLevel(BATTERY_LEVEL_EMPTY);
         }
-        vTaskDelay(10000/portTICK_PERIOD_MS);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
     }
 }
 
