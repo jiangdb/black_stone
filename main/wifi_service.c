494211ce94f85b3584cef1b0162097cf36115bc5
jiangdb <jiang.db@gmail.com>
2017-11-17 12:15:10 +0800

    Optimize wifi service

--- main/wifi_service.c
+++ main/wifi_service.c
@@ -17,37 +17,52 @@
 
 #define TAG "WIFI_SERVICE"
 
-static uint8_t wifi_status = WIFI_STATUS_UNSTART;
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+static const int CONNECTED_BIT = BIT0;
+static const int DISCONNECTED_BIT = BIT1;
+
+static uint8_t wifi_status = WIFI_STATUS_UNSTARTED;
 static TaskHandle_t xHandle = NULL;
-static SemaphoreHandle_t connectSem = NULL;
+static SemaphoreHandle_t reconnectSem = NULL;
+
+static void ws_start();
 
 esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 {
     switch(event->event_id) {
         case SYSTEM_EVENT_STA_START:
             ESP_LOGD(TAG, "SYSTEM_EVENT_STA_START");
-            wifi_status = WIFI_STATUS_STARTED;
+            wifi_status = WIFI_STATUS_CONNECTING;
             ESP_ERROR_CHECK(esp_wifi_connect());
             break;
         case SYSTEM_EVENT_STA_GOT_IP:
+            ESP_LOGD(TAG, "SYSTEM_EVENT_STA_GOT_IP");
+            ESP_LOGD(TAG, "got ip:%s\n",ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
             if (wifi_status != WIFI_STATUS_CONNECTED) {
                 wifi_status = WIFI_STATUS_CONNECTED;
                 bt_notify_wifi_status(wifi_status);
-                //setWifiSound(0, true);
                 display_seticon(ICON_WIFI, true);
             }
-            ESP_LOGD(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-            ESP_LOGD(TAG, "got ip:%s\n",ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
             break;
         case SYSTEM_EVENT_STA_DISCONNECTED:
-            if (wifi_status != WIFI_STATUS_DISCONNECTED) {
-                wifi_status = WIFI_STATUS_DISCONNECTED;
-                bt_notify_wifi_status(wifi_status);
-                //setWifiSound(0, false);
-                display_seticon(ICON_WIFI, false);
+            {
+                system_event_sta_disconnected_t *disconnected = &event->event_info.disconnected;
+                ESP_LOGD(TAG, "SYSTEM_EVENT_STA_DISCONNECTED, ssid:%s, ssid_len:%d, bssid:" MACSTR ", reason:%d", \
+                       disconnected->ssid, disconnected->ssid_len, MAC2STR(disconnected->bssid), disconnected->reason);
+                if (wifi_status != WIFI_STATUS_DISCONNECTED) {
+                    wifi_status = WIFI_STATUS_DISCONNECTED;
+                    bt_notify_wifi_status(wifi_status);
+                    display_seticon(ICON_WIFI, false);
+                }
+                //manually disconnected, do reconnect immediately
+                if (disconnected->reason == WIFI_REASON_ASSOC_LEAVE) {
+                    wifi_status = WIFI_STATUS_CONNECTING;
+                    ESP_ERROR_CHECK(esp_wifi_connect());
+                }else{
+                    xSemaphoreGive(reconnectSem);
+                }
             }
-            ESP_LOGD(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-            xSemaphoreGive(connectSem);
             break;
         default:
             break;
@@ -58,10 +73,12 @@ esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 void ws_connect_loop()
 {
     while(1) {
-        xSemaphoreTake( connectSem, portMAX_DELAY );
+        xSemaphoreTake( reconnectSem, portMAX_DELAY );
 
-        vTaskDelay(3000/portTICK_RATE_MS);
-        if (wifi_status == WIFI_STATUS_STARTED) {
+        //reconnect every 30s
+        vTaskDelay(30000/portTICK_RATE_MS);
+        if (wifi_status == WIFI_STATUS_DISCONNECTED) {
+            wifi_status = WIFI_STATUS_CONNECTING;
             ESP_ERROR_CHECK(esp_wifi_connect());
         }
     }
@@ -78,23 +95,31 @@ static bool ws_valid_config(char* ssid, char* pass)
     return true;
 }
 
-static void ws_config(char* ssid, char* pass)
+static bool ws_same_config(char* ssid, char* pass)
 {
-    esp_err_t err;
     wifi_config_t sta_config = {
         .sta = {
             .bssid_set = false
         }
     };
-
-    err = esp_wifi_get_config(WIFI_IF_STA, &sta_config);
+    esp_err_t err = esp_wifi_get_config(WIFI_IF_STA, &sta_config);
     if (err == ESP_OK) {
         ESP_LOGD(TAG, "current ssid: %s, pass %s", sta_config.sta.ssid, sta_config.sta.password);
         if ((strcmp((char*)sta_config.sta.ssid, ssid) == 0) && (strcmp((char*)sta_config.sta.password, pass) == 0)){
             ESP_LOGD(TAG, "same config");
-            return;
+            return true;
         }
     }
+    return false;
+}
+
+static void ws_config(char* ssid, char* pass)
+{
+    wifi_config_t sta_config = {
+        .sta = {
+            .bssid_set = false
+        }
+    };
 
     memset(sta_config.sta.ssid,0,sizeof(sta_config.sta.ssid));
     memcpy(sta_config.sta.ssid,ssid,strlen(ssid));
@@ -108,15 +133,29 @@ void ws_connect(char* ssid, char* pass)
 {
     if (!ws_valid_config(ssid, pass)) return;
 
-    ws_config(ssid, pass);
-    if (wifi_status == WIFI_STATUS_UNSTART) {
-        ESP_ERROR_CHECK( esp_wifi_start() );
-    } else if(wifi_status == WIFI_STATUS_STARTED) {
-        ESP_ERROR_CHECK(esp_wifi_connect());
-    } else if(wifi_status == WIFI_STATUS_CONNECTED) {
-        ESP_ERROR_CHECK(esp_wifi_disconnect());
-    } else if(wifi_status == WIFI_STATUS_DISCONNECTED) {
-        ESP_ERROR_CHECK(esp_wifi_connect());
+    switch(wifi_status) {
+        case WIFI_STATUS_UNSTARTED:
+            ws_start();
+            ws_config(ssid, pass);
+            break;
+        case WIFI_STATUS_STARTING:
+            ws_config(ssid, pass);
+            break;
+        case WIFI_STATUS_DISCONNECTED:
+            ws_config(ssid, pass);
+            wifi_status = WIFI_STATUS_CONNECTING;
+            ESP_ERROR_CHECK(esp_wifi_connect());
+            break;
+        case WIFI_STATUS_CONNECTING:
+        case WIFI_STATUS_CONNECTED:
+            if (!ws_same_config(ssid, pass)) {
+                ws_config(ssid, pass);
+                wifi_status = WIFI_STATUS_DISCONNECTING;
+                ESP_ERROR_CHECK(esp_wifi_disconnect());
+            }
+            break;
+        default:
+            break;
     }
 }
 
@@ -134,20 +173,32 @@ void ws_stop()
     esp_wifi_stop();
 }
 
-void ws_init()
+static void ws_start()
 {
+    ESP_LOGD(TAG, "%s", __func__);
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(wifi_event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_ps(WIFI_PS_MODEM) );
+    wifi_status = WIFI_STATUS_STARTING;
+    ESP_ERROR_CHECK( esp_wifi_start() );
 
-    char* wifi_name = config_get_wifi_name();
-    char* wifi_pass = config_get_wifi_pass();
-    ws_connect(wifi_name, wifi_pass);
-
-    connectSem=xSemaphoreCreateBinary();
-    //Create connect task
+    //Create re-connect task
+    reconnectSem=xSemaphoreCreateBinary();
     xTaskCreate(&ws_connect_loop, "wifi_service_connect_loop", 2048, NULL, 8, &xHandle);
+}
+
+void ws_init()
+{
+    //char* ssid = config_get_wifi_name();
+    //char* pass = config_get_wifi_pass();
+    char* ssid = "8Sian-K";
+    char* pass = "8sianmedianetwork";
+    if (!ws_valid_config(ssid, pass)) return;
+
+    ws_start();
+    ws_config(ssid, pass);
 }
